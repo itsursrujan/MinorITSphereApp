35f081e5cfe9062a9fc655ff5a8e7707db0384cf
DarshanGitHub123 <vonemaker75@gmail.com>
2024-12-08 19:32:54 +0530

    add quiz taking functionality

---
+++ app/src/main/java/com/example/minoritsphereapp/QuizPannel.kt
@@ -0,0 +1,47 @@
+package com.example.minoritsphereapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.FirebaseDatabase
+import com.example.minoritsphereapp.databinding.QuizPannelBinding
+
+class QuizPannel : AppCompatActivity() {
+    private lateinit var binding: QuizPannelBinding
+    private lateinit var quizModelList: MutableList<QuizModel>
+    private lateinit var adapter: QuizListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = QuizPannelBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quizModelList = mutableListOf()
+        getDataFromFirebase()
+    }
+
+    private fun setupRecyclerView() {
+        binding.progressBar.visibility = View.GONE
+        adapter = QuizListAdapter(quizModelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun getDataFromFirebase() {
+        binding.progressBar.visibility = View.VISIBLE
+        FirebaseDatabase.getInstance().reference
+            .get()
+            .addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    for (snapshot in dataSnapshot.children) {
+                        val quizModel = snapshot.getValue(QuizModel::class.java)
+                        if (quizModel != null) {
+                            quizModelList.add(quizModel)
+                        }
+                    }
+                }
+                setupRecyclerView()
+            }
+    }
+}
